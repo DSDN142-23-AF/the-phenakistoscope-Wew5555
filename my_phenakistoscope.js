c9b942ce71f4c102b662877299da05753396e76c
Wew_K <Ketpicpred@myvuw.ac.nz>
2024-01-11 02:11:40 +0700

    Update my_phenakistoscope.js

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -3,49 +3,70 @@ const SLICE_COUNT = 16;
 function setup_pScope(pScope) {
   pScope.output_mode(ANIMATED_DISK);
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(true); //line on circle show
+  pScope.draw_layer_boundaries(false); //line on circle show
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
   pScope.load_image("pirate_ship", "png");
+  pScope.load_image("wave", "png");
+  pScope.load_image("wave2", "png");
+  pScope.load_image("wave3","png");
 }
 
 function setup_layers(pScope) {
   new PLayer(); //lets us draw the whole circle background, ignoring the boundaries
 
+  //shy
   let sky = new PLayer(theSky);
   sky.mode(RING);
 
+  //sea layer
   let sea = new PLayer(wave);
   sea.mode(RING);
 
+  let sea2 = new PLayer(waveL2);
+  sea2.mode(RING);
+
+  let sea3 = new PLayer(waveL3);
+  sea3.mode(RING);
+
+  //the sun
   let skySun = new PLayer(sun);
   skySun.mode(SWIRL(1));
-  skySun.set_boundary(5,15);
-
+  skySun.set_boundary(5, 15);
+  
+  //the ship
   let pirateShip = new PLayer(ship);
   pirateShip.mode(SWIRL(1));
   pirateShip.set_boundary(500, 510); //where the image start and stop.
 }
 
 function ship(x, y, animation, pScope) {
-  
-  pScope.draw_image("pirate_ship", x, y);
+  let shipX = animation.wave(1) * 50;
+  pScope.draw_image("pirate_ship", shipX, y);
 }
 
 function wave(x, y, animation, pScope) {
-  let angleOffset = (360 / SLICE_COUNT) / 2
-  let backgroundArcStart = 270 - angleOffset;
-  let backgroundArcEnd = 270 + angleOffset;
-  //sea
-  fill(69, 212, 245);
-  arc(x, y, 800, 800, backgroundArcStart, backgroundArcEnd); // draws "pizza slice" in the background
   //the wave
-  fill(255)
-  rect(-10, -300 - animation.wave() * 50, 20, 20) // .wave is a cosine wave btw
+  scale(0.5);
+  let waveX = animation.wave(1) * -50;
+  pScope.draw_image("wave", waveX, -665);
+}
+
+function waveL2(x, y, animation, pScope) {
+  //the second wave
+  scale(0.5);
+  let wave2X = animation.wave(1) * -50;
+  pScope.draw_image("wave2", wave2X, -500);
+}
+
+function waveL3(x,y,animation,pScope){
+  //the third wave
+  scale(0.4)
+  let wave3X = animation.wave(1)*+50;
+  pScope.draw_image("wave3",wave3X, -400);
 }
 
 function theSky(x, y, animation, pScope) {
-  // this is how you set up a background for a specific layer
   let angleOffset = (360 / SLICE_COUNT) / 2
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
@@ -53,22 +74,16 @@ function theSky(x, y, animation, pScope) {
   scale(2.5)
   fill(204, 254, 255); //blue background
   arc(x, y, 800, 800, backgroundArcStart, backgroundArcEnd); // draws "pizza slice" in the background
-  //white part
-
-  //scale(0.7);// the second layer, where the ship is.
-  //fill(204,254,255); //blue background
-  //arc(x, y, 800, 800, backgroundArcStart, backgroundArcEnd); // draws "pizza slice" in the background
-
 }
 
 function sun(x, y, animation, pScope) {
   noStroke();
-  fill(255,255,255);
-  circle(0,900,180);
-  
-  fill(255,255,200);
+  fill(255, 255, 255);
+  circle(0, 900, 180);
+
+  fill(255, 255, 200);
   circle(0, 900, 140);
 
-  fill(255,255,0);
+  fill(255, 255, 0);
   circle(0, 900, 95);
 }
